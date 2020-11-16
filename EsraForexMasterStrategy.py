9af3f768f1f8bd854b7a68207e8a9dfe29214d0e
Ninaad Lakshman <ninaadlakshman@MacBook-Pro.local>
2020-11-16 11:29:58 -0500

    Added Buy/Sell + Open position Trading

--- EsraForexMasterStrategy.py
+++ EsraForexMasterStrategy.py
@@ -23,8 +23,9 @@ import oandapyV20.endpoints.pricing as pricing
 import oandapyV20.endpoints.trades as trades
 import oandapyV20.endpoints.transactions as transactions
 from oandapyV20.contrib.requests import MarketOrderRequest
+from oandapyV20.contrib.requests import TrailingStopLossOrderRequest
 import trendln
-
+import lstmpredictor
 
 #account info for live acct.
 # accountID = "001-001-4630515-001"
@@ -42,10 +43,33 @@ fee_pct = 0.0 # YESSIRRR OANDA IS FREE COMMISSION
 current = datetime.datetime.now()
 current_time = current.time()
 
-async def run():
-    print("Starting master strategy... \n")
+#first order since bot start info
+first_order = True
+first_order_id = "63"
+
+#pandas dataframe where we will be logging realized P/L for all closed orders in real time
+trade_log = pd.DataFrame(
+        columns = ['_id', 'market', 'position_open_time', 'position_close_time', 'realized_pl_usd', 'realized_pl_pct', 'success']
+    )
+#Open Positions
+open_positions = positions.OpenPositions(accountID = accountID) #OLDEST TO NEWEST TRANSACTION
+oanda.request(open_positions)
+
+#buy/sell indicator values for opening a long or short position
+long_buy = 3.5
+long_sell = 2.5
+short_buy = 2.5
+short_sell = 3.5
+
+async def run(loop):
+    print("\n---------------------Starting master strategy...------------------- \n")
     await asyncio.sleep(3)
-    scores_by_market = await scalp()
+    await asyncio.gather(
+            scalp(),
+            watchman()
+        )
+
+
 
 ### Master Strategy Stuff ###
 
@@ -57,6 +81,7 @@ async def run():
 #    4. add points of value strategy
 #    5. get feedback - log P/L's and strategy scores for each position once it is closed. This will be used as a learner.
 #    6. add Randomized Weighted Majority Algorithm to optimize weights for strategies over time
+#    7. Exponential Trailing stop loss
 
 async def scalp(order_mode=False):
     instruments1 = oanda.request(accounts.AccountInstruments(accountID))["instruments"]
@@ -74,16 +99,38 @@ async def scalp(order_mode=False):
         MACD_score = await MACD_strategy(market, "H1")
         ATR_score = await ATR_strategy(market, "H1")
         points_of_val_score = await points_of_value(market, "H1")
-        print("MARKET: {} \n".format(market))
+
+        ### This section is for LSTM Neural Network that will predict the next price
+        params = {
+            "count": 500,
+            "granularity": "H1"
+            }
+        candles = oanda.request(instruments.InstrumentsCandles(instrument=market, params=params))['candles']
+        df = pd.DataFrame(
+                columns=["Date", "Open", "High", "Low", "Close", "Volume"]
+            )
+        df['Date'] = [i['time'] for i in candles]
+        df['Open'] = [float(i['mid']['o']) for i in candles]
+        df['High'] = [float(i['mid']['h']) for i in candles]
+        df['Low'] = [float(i['mid']['l']) for i in candles]
+        df['Close'] = [float(i['mid']['c']) for i in candles]
+        df['Volume'] = [float(i['volume']) for i in candles]
+        last_val, next_val = lstmpredictor.lstm_neural_network(df)
+        print(last_val, next_val)
+        ###
+
+        print("\nMARKET: {}".format(market))
         print("RSI score: {}".format(rsi_h1_score))
         print("double EMA score: {}".format(double_ema_score))
         print("MACD score: {}".format(MACD_score))
-        print("ATR score: {}".format(MACD_score))
+        print("ATR score: {}".format(ATR_score))
+        print("Points of Value score: {}".format(points_of_val_score))
         scores_by_market[i]['strategy_scores'].extend([rsi_h1_score, double_ema_score, MACD_score, ATR_score])
         overallscore = {}
         buy = 0
         sell = 0
         count = 0
+        ## Apply Randomized Weighted Majority Algo here to optimize the weights of each strategy over time
         for strategyscore in scores_by_market[i]['strategy_scores']:
             try:
                 buy += strategyscore['buy_signal']
@@ -99,17 +146,106 @@ async def scalp(order_mode=False):
         overallscore['buy_signal'] = buy/count
         overallscore['sell_signal'] = sell/count
         scores_by_market[i]['overall_score'] = overallscore
-        if (overallscore['buy_signal'] > 3.5 and overallscore['sell_signal'] < 2.5) or (overallscore['buy_signal'] < 2.5 and overallscore['sell_signal'] > 3.5):
-            print("\nFOUND PROFITABLE TRADE OPPORTUNITY---------------")
+        if (overallscore['buy_signal'] > long_buy and overallscore['sell_signal'] < long_sell):
+            print("\nFOUND LONG PROFITABLE TRADE OPPORTUNITY---------------")
+            buy(market, 'long', 10) #10 refers to amount of money wanted to invest
             print("OVERALL SCORE: {}".format(overallscore))
             if order_mode:
-                market_order = await asyncio.ensure_future(create_order(scores_by_market[i]['market'], overallscore['buy_signal'], overallscore['sell_signal']))
+                market_order, trailing_stop = await asyncio.ensure_future(create_order(scores_by_market[i]['market'], overallscore['buy_signal'], overallscore['sell_signal']))
+                order_id = market_order['orderFillTransaction']['orderID']
+                if first_order:
+                    first_order_id = order_id
+                    first_order = False
+                await asyncio.ensure_future(watch_order(order_id))
+        elif (overallscore['buy_signal'] < short_buy and overallscore['sell_signal'] > short_sell):
+            print("\nFOUND SHORT PROFITABLE TRADE OPPORTUNITY---------------")
+            buy(market, indicators_dict, 'short')
+            print("OVERALL SCORE: {}".format(overallscore))
         i += 1
     return scores_by_market
 
 
+#Watchman function watches transaction history continuosly, in parallel, to ensure nothing crazy happens. Also logs all the orders and realized/unrealized P/L.
+#UPDATE - Logging P/L complete. Need to finish alert system although with the trailing stop I dont think there will be a need.
+async def watchman():
+    while 1:
+        params = {
+            "sinceTransactionID": int(first_order_id)
+        }
+        account_info = oanda.request(accounts.AccountChanges(accountID, params=params))
+        account_state = account_info['state']
+        account_changes = account_info['changes']
+        open_positions = account_state['positions']
+        recently_opened_positions = account_changes['tradesOpened']
+        recently_closed_positions = account_changes['tradesClosed']
+        #add recent additions to trade log
+        recent_additions = []
+        for closed_pos in recently_closed_positions:
+            if closed_pos['realizedPL'][0] == '-':
+                success = False
+            else:
+                success = True
+            pct = float(closed_pos['realizedPL'])/float(closed_pos['initialUnits'])
+            recent_additions.append([closed_pos['id'], closed_pos['instrument'], closed_pos['openTime'], str(datetime.datetime.now()), closed_pos['realizedPL'], str(pct), success])
+        for update in recent_additions:
+            update_df = pd.DataFrame(
+                    data=update,
+                    columns=['_id', 'market', 'position_open_time', 'position_close_time', 'realized_pl_usd', 'realized_pl_pct', 'success']
+                )
+            trade_log.append(update_df, ignore_index=True)
+        # print(trade_log)
+        #NEEDS TO BE DONE - Watch for huge losses, incoming margin closeout, or any other discrepencies     
+        await asyncio.sleep(3)
+    pass
 
-#Function to order based on overall score for 1 market
+
+#function that monitors price for a market that we've opened a new position for and sells when it detects reversal
+async def watch_order(order_id):
+    #needs to be completed
+    pass
+
+def buy(market, positionType, positionSize): #positionSize in Dollars (Amount of Money Plan on Investing)
+    if (positionType == 'long'):
+        response = oanda.get_prices(instruments=market)
+        prices = response.get("prices")
+        bidding_price = prices[0].get("bid")
+        buy_units = (int) (positionSize / bidding_price);
+
+        trade_expire = datetime.utcnow() + timedelta(days=1)
+        trade_expire = trade_expire.isoformat("T") + "Z"
+        response = oanda.create_order(account_id,
+            instrument=market,
+            units=buy_units,
+            side='buy',
+            type='limit',
+            price=bidding_price,
+            expiry=trade_expire
+        )
+    else:
+        response = oanda.get_prices(instruments=market)
+        prices = response.get("prices")
+        asking_price = prices[0].get("ask")
+        sell_units = (int) (positionSize / asking_price);
+
+        trade_expire = datetime.utcnow() + timedelta(days=1)
+        trade_expire = trade_expire.isoformat("T") + "Z"
+        response = oanda.create_order(account_id,
+            instrument=market,
+            units=sell_units,
+            side='sell',
+            type='limit',
+            price=asking_price,
+            expiry=trade_expire
+        )
+
+         
+def findOpenPositionMarkets():
+    markets = []
+    for i in range(0, len(open_positions.response.get('positions'))):
+        markets.append(open_positions.response.get('positions')[i].get('instrument', 0))
+    return markets
+
+#Function to order based on overall score for 1 market - NEED to finish expononential trailing stop and reducing order size as order goes in our direction (hedging)
 async def create_order(market, buy_signal, sell_signal):
     # if buy_signal > sell_signal:
     #     mo = MarketOrderRequest(instrument=market, units=(buy_signal * 10000 - sell_signal * 10000))
@@ -120,7 +256,14 @@ async def create_order(market, buy_signal, sell_signal):
     market_order = oanda.request(orders.OrderCreate(accountID, data=mo.data))
     #order confirmation
     print(json.dumps(market_order, indent=4))
-    return market_order
+    #set trailing stop
+    order_id = market_order['orderFillTransaction']['orderID']
+    ordr = TrailingStopLossOrderRequest(tradeID=order_id, distance=20)
+    trailing_stop = oanda.request(orders.OrderCreate(accountID, data=ordr.data))
+    #trailing stop confimrmation
+    print(json.dumps(trailing_stop, indent=4))
+
+    return market_order, trailing_stop
 
 
 
@@ -169,46 +312,75 @@ async def double_ema(market, length):
         general_trend = 'neutral'
 
     #find candle of most recent intersection
-    emas_short = (await calc_emas(candles, 5))[-1:0:-1]
-    emas_medium = (await calc_emas(candles, 15))[-1:0:-1]
+    emas_short = (await calc_emas(candles, 5)).tolist()[-1:0:-1]
+    emas_medium = (await calc_emas(candles, 15)).tolist()[-1:0:-1]
+    emas_long = (await calc_emas(candles, 75)).tolist()[-1:0:-1]
     i = 0
     direction = ""
     for a in range(len(emas_medium)):
-        if emas_short[a + 10] < emas_medium[a]:
+        if emas_short[a] < emas_medium[a]:
             if i == 1:
                 direction = "Crossed Bearish"
-                intersect_candle = (candles[-1:0:-1])[a + 15]
-                buy_signal += 4
+                intersect_candle = (candles[-1:0:-1])[a]
+                buy_signal += 3
                 break
             i = -1
-        elif emas_short[a + 10] > emas_medium[a]:
+        elif emas_short[a] > emas_medium[a]:
             if i == -1:
                 direction = "Crossed Bullish"
-                intersect_candle = (candles[-1:0:-1])[a + 15]
-                sell_signal += 4
+                intersect_candle = (candles[-1:0:-1])[a]
+                sell_signal += 3
                 break
             i = 1
         else:
-            intersect_candle = (candles[-1:0:-1])[a + 15]
+            intersect_candle = (candles[-1:0:-1])[a]
             if i == -1:
                 direction = "Crossed Bearish"
-                buy_signal += 4
+                buy_signal += 3
                 break
             elif i == 1:
                 direction = "Crossed Bullish"
-                sell_signal += 4
+                sell_signal += 3
+                break
+    direction1 = ""
+    intersect_candle1 = []
+    for a in range(len(emas_medium)):
+        if emas_medium[a] < emas_long[a]:
+            if i == 1:
+                direction1 = "Crossed Bearish"
+                intersect_candle1 = (candles[-1:0:-1])[a]
+                buy_signal += 3
+                break
+            i = -1
+        elif emas_medium[a] > emas_long[a]:
+            if i == -1:
+                direction1 = "Crossed Bullish"
+                intersect_candle1 = (candles[-1:0:-1])[a]
+                sell_signal += 3
+                break
+            i = 1
+        else:
+            intersect_candle1 = (candles[-1:0:-1])[a]
+            if i == -1:
+                direction1 = "Crossed Bearish"
+                buy_signal += 3
+                break
+            elif i == 1:
+                direction1 = "Crossed Bullish"
+                sell_signal += 3
                 break
 
     #checks volume of intersection candle to see if "power move" is present
-    if len(intersect_candle) == 0:
-        print("No double EMA intersection found recently")
-        return None
-    candle_vol = float(intersect_candle['mid']['o']) - float(intersect_candle['mid']['c'])
-    if (candle_vol/float(intersect_candle['mid']['o'])) * 100 > 1:
+    if len(intersect_candle1) == 0:
+        print("No EMA 15 - EMA 75 intersection found recently")
+        if len(intersect_candle) == 0:
+            print('No EMA 5 - EMA 15 intersection found recently')
+            return None
+    if direction1 == direction:
         if direction == "Crossed Bullish":
-            buy_signal += 4
+            sell_signal += 2
         elif direction == "Crossed Bearish":
-            sell_signal += 4
+            buy_signal += 2
     return {
         'trend': general_trend,
         'buy_signal': buy_signal,
@@ -354,7 +526,7 @@ async def points_of_value(market, length):
         "granularity": length
     }
     candles = oanda.request(instruments.InstrumentsCandles(instrument=market, params=params))['candles']
-    await support_resistance(market, candles)
+    #await support_resistance(market, candles)
     return {
         'enter_signal': enter_signal,
         'exit_signal': exit_signal
@@ -368,37 +540,12 @@ async def points_of_value(market, length):
 
 #Calculates EMA's for each set of candles (ie if you plot it it would be the EMA)
 async def calc_emas(candles, length):
-    emas = []
-    i = 0
-    num_first = 0
-    sum_first = 0
-    first_after_period = True
-    prev_ema = 0
-    for candle in candles[-1:0:-1]:
-        if i <= length:
-            sum_first += float(candle['mid']['c'])
-            num_first += 1
-            i += 1
-            continue
-        if first_after_period:
-            sma = sum_first/num_first
-            ema = await calc_ema(length, float(candle['mid']['c']), sma)
-            prev_ema = ema
-            emas.append(ema)
-            first_after_period = False
-        else:
-            ema = await calc_ema(length, float(candle['mid']['c']), prev_ema)
-            emas.append(ema)
-            prev_ema = ema
+    close_prices = [float(i['mid']['c']) for i in candles]
+    df = pd.Series(close_prices)
+    emas = df.ewm(span=length).mean()
     #print(emas)
     return emas
 
-
-#calculates ema for a particular candle
-async def calc_ema(length, curr_price, prev_ema):
-    weight_factor = 2/(length + 1)
-    ema = weight_factor * (curr_price - prev_ema) + prev_ema
-    return ema
 
 
 #Calculates Bollinger Bands given candlesticks
@@ -489,8 +636,8 @@ async def support_resistance(market, candles):
     data = np.array(close_prices)
     close_series = pd.Series(data)
     (minimaIdxs, pmin, mintrend, minwindows), (maximaIdxs, pmax, maxtrend, maxwindows) = trendln.calc_support_resistance(close_series)
-    fig = trendln.plot_support_resistance(close_series)
-    # plt.savefig('suppres_{}_{}.svg'.format(market, str(datetime.now())), format='svg')
+    fig = trendln.plot_support_resistance(close_series, numbest=2)
+    # plt.savefig('suppres_{}_{}.svg'.format(market, str(datetime.datetime.now())), format='svg')
     # plt.show()
     plt.clf()
     return minimaIdxs, pmin, mintrend, minwindows, maximaIdxs, pmax, maxtrend, maxwindows
@@ -520,7 +667,8 @@ async def ATR(market, candles, length):
 
 
 
-
-asyncio.get_event_loop().run_until_complete(run())
+open_positions_watchlist = findOpenPositionMarkets()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(loop))
 if __name__ == "__main__":
         run()
